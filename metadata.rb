f5e0980b00467713cc13ee059dff9461d9ac8c4c
Baptiste Courtois <b.courtois@criteo.com>
2018-10-11 09:12:05 +0200

    Fix foodcritic violation for Chef 12 version
    Update the Rakefile to use Chef 12 compatible foodcritic rules.
    Fix violations in metadata.
    Remove useless use of updated_by_last_action since we use converge_by.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name             'winrm-config'
 maintainer       'Criteo'
 maintainer_email 'b.courtois@criteo.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Configures winrm service and client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.4'
 supports         'windows', '>= 6.0'
 depends          'windows', '>= 2.1'
 chef_version     '>= 12.1' if respond_to?(:chef_version)
+issues_url       'https://github.com/criteo-cookbooks/winrm-config/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/criteo-cookbooks/winrm-config' if respond_top?(:source_url)
